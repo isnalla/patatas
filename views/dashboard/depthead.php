4299faa907b879e28d9598da3a420539b68d83e5
KJCA <kimjoshuaadvincula@gmail.com>
2013-10-17 02:49:59 +0800

    Sorting gradesheets

--- views/dashboard/depthead.php
+++ views/dashboard/depthead.php
@@ -11,81 +11,118 @@
     </div>
 
     <div id="gradesheets_container"></div>
+    <div id="gradesheets_container_approved"></div>
+    <div id="gradesheets_container_disapproved"></div>
     <h3 id="grades_info"></h3>
     <div id="grades_container"></div>
 
     <script>
+        var gradesheets = [];
+
         $(function(){
-            search_gradesheet();
+            search_gradesheet("gradesheets_container", "PENDING",1);
+            search_gradesheet("gradesheets_container_approved", "APPROVED",2);
+            search_gradesheet("gradesheets_container_disapproved","DISAPPROVED",3);
             $('#search_button').click(function(e){
                 search_gradesheet();
                 e.preventDefault();
             });
         });
 
-        function search_gradesheet(){
+        function search_gradesheet(container,status,index){
 
-            data = {'course_code':$('#search_text').val()};
+            data = {'course_code':$('#search_text').val(),'status':status};
 
             $.post("/logic/depthead.php",{'method':'get_gradesheets', 'data':data},function(data){
 
-            //    console.log(data);
                 data = JSON.parse(data);
+                gradesheets[index]= data;
+                show_gradesheets(container,index);
+            });
 
+        }
 
-                $("#gradesheets_container").html(
-                    "<table id=\"gradesheets_table\">" +
-                        "<tr>" +
-                        "<th>Subject</th>" +
-                        "<th>Section</th>" +
-                        "<th>Lecturer</th>" +
-                        "<th>Status</th>" +
-                        "</tr>"
+        function show_gradesheets(container,index){
+            $("#"+container).html(
+                "<table id=\"gradesheets_table\">" +
+                    "<tr>" +
+                    "<th>Subject</th>" +
+                    "<th>Section</th>" +
+                    "<th>Lecturer</th>" +
+                    "<th>Status</th>" +
+                    "</tr>"
+            );
+
+            console.log(gradesheets[index]);
+
+            for(i = 0, j = gradesheets[index].length; i<j; i++){
+                $("#"+container+" > table").append(
+                    "<tr value = '"+gradesheets[index][i].Section+"'>" +
+                        "<td>" +
+                        gradesheets[index][i].Course_code +
+                        "</td>" +
+                        "<td>" +
+                        gradesheets[index][i].Section +
+                        "</td>" +
+                        "<td>" +
+                        gradesheets[index][i].Lecturer +
+                        "</td>" +
+                        "<td>" +
+                        gradesheets[index][i].Status +
+                        "</td>"
+//                    "</tr>"
                 );
 
-
-                for(i = 0, j = data.length; i<j; i++)
-                    $("#gradesheets_container > table").append(
-                        "<tr value = '"+data[i].Section+"'>" +
-                            "<td>" +
-                            data[i].Course_code +
-                            "</td>" +
-                            "<td>" +
-                            data[i].Section +
-                            "</td>" +
-                            "<td>" +
-                            data[i].Lecturer +
-                            "</td>" +
-                            "<td>" +
-                            data[i].Status +
-                            "</td>" +
-                            "<td>" +
-                            "<button onclick = 'update_gradesheet("+'"'+data[i].Section+'"'+","+'"'+data[i].Course_code+'"'+","+'"'+"APPROVED"+'"'+")'>Approve</button>" +
-                            "</td>" +
-                            "<td>" +
-                            "<button onclick = 'update_gradesheet("+'"'+data[i].Section+'"'+","+'"'+data[i].Course_code+'"'+","+'"'+"DISAPPROVED"+'"'+")'>Disapprove</button>" +
-                            "</td>" +
-                            "</form>" +
-                        "</tr>"
+                if(container == "gradesheets_container"){
+                    $("#"+container+" > table > tbody >tr:nth-child(" + (i+1) + ")").next().append(
+                        "<td>" +
+                        "<button onclick = 'update_gradesheet("+'"'+gradesheets[index][i].Section+'"'+","+'"'+gradesheets[index][i].Course_code+'"'+","+'"'+"APPROVED"+'"'+")'>Approve</button>" +
+                        "</td>" +
+                        "<td>" +
+                        "<button onclick = 'update_gradesheet("+'"'+gradesheets[index][i].Section+'"'+","+'"'+gradesheets[index][i].Course_code+'"'+","+'"'+"DISAPPROVED"+'"'+")'>Disapprove</button>" +
+                        "</td>"
                     );
+                }
 
-                $("#gradesheets_container").append("</table>");
+               $("#"+container+" > table > tbody").append();
+            }
 
-                $('#gradesheets_table').find('tr').next().on('click',function(){
+                $("#"+container).append("</table>");
+
+                $("#"+container+" > table").find('tr').next().on('click',function(){
                     show_grades($(this).attr('value'),$(this).children("td").next().next().html());
                 });
 
-
-//add grades to the gradesheet table
+                $("#"+container+" > table").find('tr').find('th').on('click', function(){
+                    arrange_gradesheets($(this).html(),$(this).parent().parent().parent().parent().attr('id'),index);
+                });
+    //add grades to the gradesheet table
                 //make it hidden at first and could be shown when clicked like in toggleable asdfasdf
+        }
+
+        function arrange_gradesheets(arrange_by, parent_id,index){
+            gradesheet = gradesheets[index].sort(function(a,b){
+                if(arrange_by.toLowerCase() == 'lecturer'){
+                    return a.Lecturer > b.Lecturer;
+                }
+                else if(arrange_by.toLowerCase() == 'subject'){
+                    return a.Course_code > b.Course_code;
+                }
+                else if(arrange_by.toLowerCase() == 'status'){
+                    return a.Status > b.Status;
+                }
             });
+
+            show_gradesheets(parent_id,index);
         }
 
         function update_gradesheet(section, course_code, status){
             data = {'course_code':course_code, 'section':section, 'status':status};
 
             $.post("/logic/depthead.php/",{'method':'update_gradesheet', 'data':data},function(data){
-                search_gradesheet();
+                search_gradesheet("gradesheets_container", "PENDING",1);
+                search_gradesheet("gradesheets_container_approved", "APPROVED",2);
+                search_gradesheet("gradesheets_container_disapproved","DISAPPROVED",3);
 
                 //    console.log(data);
 //                data = JSON.parse(data);
@@ -110,7 +147,6 @@
                         "<th>Student No</th>" +
                         "<th>Grade</th>" +
                         "<th>Remarks</th>" +
-                        "<td><input type=\"button\" id=\"add_button\" value=\"Add Grade\" /></td>"+
                         "</tr>"
                 );
 
@@ -126,10 +162,6 @@
                             "</td>" +
                             "<td>" +
                             "<input type=\"text\" value=\""+ data[i].Remarks+"\" />" +
-                            "</td>" +
-                            "<td>" +
-                            "<input type=\"button\" id=\"edit_button\" value=\"Save Changes\" />"+
-                            "<input type=\"button\" id=\"delete_button\" value=\"X\" />"+
                             "</td>" +
                             "</tr>"
                     );
